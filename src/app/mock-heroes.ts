e68b3df83b517c727aa09c5e44e962053e3fe336
lorena <lorena@salsalab.mx>
2021-10-28 10:34:35 -0500

    cambiar heroes

--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -3,13 +3,13 @@ import { Hero } from './hero';
 
 export const HEROES: Hero[] = [
     
-  { id: 12, name: 'Alfa' },
-  { id: 13, name: 'Beta' },
-  { id: 14, name: 'Gama' },
-  { id: 15, name: 'Delta' },
-  { id: 16, name: 'Epsilon' },
-  { id: 17, name: 'Zeta' },
-  { id: 18, name: 'Iota' },
-  { id: 19, name: 'Kapa' },
-  { id: 20, name: 'Lamda' }
+  { id: 12, name: 'Vicente Guerrero  ' },
+  { id: 13, name: 'Francisco Javier Mina' },
+  { id: 14, name: 'Nicolás Bravo' },
+  { id: 15, name: 'Juan Aldama' },
+  { id: 16, name: 'Ignacio Allende Unzaga' },
+  { id: 17, name: 'Josefa Ortiz de Domínguez' },
+  { id: 18, name: 'José María Morelos y Pavón' },
+  { id: 19, name: 'Mariano Matamoros' },
+  { id: 20, name: 'Guadalupe Victoria' }
 ];
